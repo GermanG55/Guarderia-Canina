bc7251848de029a0932de0f686f0719163a8573a
GermanG55 <grmnrtr@gmail.com>
2023-03-25 19:00:39 -0500

    DTOs formados

--- src/main/java/com/example/demo/controller/dto/BookingDTO.java
+++ src/main/java/com/example/demo/controller/dto/BookingDTO.java
@@ -1,2 +1,29 @@
-package com.example.demo.controller.dto;public class BookingDTO {
+package com.example.demo.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@Data
+@Table(name = "BOOKING")
+public class BookingDTO {
+
+    @Id
+    @Column(name = "ID", nullable = false, length = 10)
+    private Integer booking_id;
+
+    @Column(name = "CLIENT_ID", nullable = false, length = 10)
+    private Integer client_id;
+    //@ForeignKey
+    @Column(name = "PET_ID", nullable = false, length = 10)
+    private Integer pet_id;
+
+    @Column(name = "DATE", nullable = false, length = 50)
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
 }
