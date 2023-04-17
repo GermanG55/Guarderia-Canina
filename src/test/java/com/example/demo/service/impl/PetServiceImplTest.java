2643d5fb9187c1c3e57388935fbf792ccd28c24c
GermanG55 <grmnrtr@gmail.com>
2023-04-17 16:30:38 -0500

    "Corrgie pruebas unitarias excepto Date debido a error con la clase"

--- src/test/java/com/example/demo/service/impl/PetServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/PetServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.model.Pet;
+import com.example.demo.model.User;
 import com.example.demo.repository.PetRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -24,63 +25,64 @@ public class PetServiceImplTest {
 
     @Test
     public void Given_A_Pet_When_Save_Pet_Is_Cast_Then_Return_True(){
-        //arrange
+
         Pet pet = new Pet(1,"Michi",1,null,new java.util.Date());
         Mockito.when(petRepository.save(any(Pet.class))).thenReturn(pet);
         Mockito.when(petRepository.countByClient(any(Integer.class))).thenReturn(1);
-        //act
+
         Boolean response = petServiceImpl.savePet(pet);
-        //assert
+
         Assertions.assertNotNull(response);
         Assertions.assertEquals(true,response);
-        Mockito.verify(petRepository).save(any(Pet.class));
+        Mockito.verify(petRepository).save(pet);
     }
     @Test
     public void Given_A_Pet_When_Save_Pet_Is_Cast_And_The_Condition_Is_False_Then_Return_False(){
-        //arrange
+
         Pet pet = new Pet(1,"Michi",1,null,new java.util.Date());
         Mockito.when(petRepository.countByClient(any(Integer.class))).thenReturn(3);
-        //act
+
         Boolean response = petServiceImpl.savePet(pet);
-        //assert
+
         Assertions.assertNotNull(response);
         Assertions.assertEquals(false,response);
         Mockito.verify(petRepository).countByClient(any(Integer.class));
     }
     @Test
     public void Given_A_Pet_And_An_Illegal_Exception_When_Save_Pet_Is_Cast_Then_Return_False(){
-        //arrange
+
         Pet pet = new Pet(1,"Michi",1,null,new java.util.Date());
         Mockito.when(petRepository.save(any(Pet.class))).thenThrow(new IllegalArgumentException());
-        //act
+
         Boolean response = petServiceImpl.savePet(pet);
-        //assert
+
         Assertions.assertNotNull(response);
         Assertions.assertEquals(false,response);
     }
     @Test
-    public void Given_A_Pet_List_When_Find_All_Is_Cast_Then_Return_Pet_List(){
-        //arrange
-        List<Pet> PetList = new ArrayList<>();
-        Pet pet = new Pet(1,"Michi",1,null,new java.util.Date());
-        PetList.add(pet);
-        Mockito.when(petRepository.findAll()).thenReturn(PetList);
-        //act
+    public void Given_A_Pet_List_When_Find_All_Is_Cast_Then_Return_Pet_List() {
+
+        List<Pet> petList = new ArrayList<>();
+        Pet pet = new Pet(1, "Michi", 1, null, new java.util.Date());
+        petList.add(pet);
+        Mockito.when(petRepository.findAll()).thenReturn(petList);
+
         List<Pet> response = petServiceImpl.findAll();
-        //assert
+
         Assertions.assertNotNull(response);
         Mockito.verify(petRepository).findAll();
     }
     @Test
     public void Given_A_List_And_An_Owner_Document_When_Find_By_Owner_Is_Cast_Then_Return_Pet_List(){
-        List<Pet> PetList = new ArrayList<>();
-        Pet pet = new Pet(1,"Michi",1,null,new java.util.Date());
-        PetList.add(pet);
-        Mockito.when(petRepository.findByOwnerDocument(any(Integer.class))).thenReturn(PetList);
-        //act
-        List<Pet> response = petServiceImpl.findAll();
-        //assert
+
+        List<Pet> petList = new ArrayList<>();
+        Pet pet = new Pet(1,"German",1,new User(),new java.util.Date());
+        petList.add(pet);
+        Mockito.when(petRepository.findByOwnerDocument(1)).thenReturn(petList);
+
+        List<Pet> response = petServiceImpl.findByOwnerDocument(1);
+
         Assertions.assertNotNull(response);
-        Mockito.verify(petRepository).findByOwnerDocument(any(Integer.class));
+        Mockito.verify(petRepository).findByOwnerDocument(1);
     }
 }
--- src/test/java/com/example/demo/service/impl/UserServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/UserServiceImplTest.java
@@ -17,46 +17,71 @@ import static org.mockito.ArgumentMatchers.any;
 
 
 @ExtendWith(MockitoExtension.class)
-public class UserServiceImplTest {
+public class
+UserServiceImplTest {
     @InjectMocks
     private UserServiceImpl userServiceImpl;
     @Mock
     private UserRepository userRepository;
     @Test
     public void Given_A_User_When_Save_User_Is_Cast_Then_Return_True(){
-        //arrange
+
         User user = new User(1,"German","Negro Arroyo",new java.util.Date());
         Mockito.when(userRepository.save(any(User.class))).thenReturn(user);
-        //act
+
         Boolean response = userServiceImpl.saveUser(user);
-        //assert
+
         Assertions.assertNotNull(response);
         Assertions.assertEquals(true,response);
         Mockito.verify(userRepository).save(any(User.class));
     }
     @Test
     public void Given_A_User_And_An_Illegal_Exception_When_Save_User_Is_Cast_Then_Return_False(){
-        //arrange
+
         User user = new User(1,"German","Negro Arroyo",new java.util.Date());
         Mockito.when(userRepository.save(any(User.class))).thenThrow(new IllegalArgumentException());
-        //act
         Boolean response = userServiceImpl.saveUser(user);
-        //assert
+
         Assertions.assertNotNull(response);
         Assertions.assertEquals(false,response);
     }
     @Test
     public void Given_A_User_List_When_Find_All_Is_Cast_Then_Return_User_List(){
-        //arrange
+
         List<User> userList = new ArrayList<>();
         User user = new User(1,"German","Negro Arroyo",new java.util.Date());
         userList.add(user);
         Mockito.when(userRepository.findAll()).thenReturn(userList);
-        //act
+
         List<User> response = userServiceImpl.findAll();
-        //assert
+
         Assertions.assertNotNull(response);
         Mockito.verify(userRepository).findAll();
     }
+    @Test
+    public void Given_A_User_List_When_Find_Booking_By_Document_Is_Cast_Then_Return_Booking_List(){
 
+        List<User> userList = new ArrayList<>();
+        User user = new User(1,"German","Negro Arroyo",new java.util.Date());
+        userList.add(user);
+        Mockito.when(userRepository.findBookingByDocument(any(Integer.class))).thenReturn(userList);
+
+        List<User> response = userServiceImpl.findBookingByDocument(1);
+
+        Assertions.assertNotNull(response);
+        Mockito.verify(userRepository).findBookingByDocument(1);
+    }
+    @Test
+    public void Given_A_User_List_When_Find_Pets_By_Document_Is_Cast_Then_Return_Pet_List(){
+
+        List<User> userList = new ArrayList<>();
+        User user = new User(1,"German","Negro Arroyo",new java.util.Date());
+        userList.add(user);
+        Mockito.when(userRepository.findPetsByDocument(any(Integer.class))).thenReturn(userList);
+
+        List<User> response = userServiceImpl.findPetsByDocument(1);
+
+        Assertions.assertNotNull(response);
+        Mockito.verify(userRepository).findPetsByDocument(1);
+    }
 }
